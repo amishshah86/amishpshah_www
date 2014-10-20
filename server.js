292e191c30a6518d7879b7c9051a52207f4ac8b9
Amish Shah <amish@itibit.com>
2014-10-20 14:00:07 -0700

    basic project boilerplate set up

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+/*
+	|	Amish Shah
+	|	www.amishpshah.com
+	|	amishshah86@gmail.com
+*/
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic('public')).listen(8888);
+console.log('Server running at http://127.0.0.1:8888/')
